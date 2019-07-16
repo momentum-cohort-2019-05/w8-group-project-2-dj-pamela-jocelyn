f9d8bbba922024d5e825decf4886f1c0afb95890
polynova <jocelyncasanova1@gmail.com>
2019-07-16 16:22:47 -0400

    made question and answer models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Question(models.Model):
+    """       """
+    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=500)
+
+    def __str__(self):
+        """    """
+        return self.body
+
+class Answer(models.Model):
+    """      """
+    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+
+    def __str__(self):
+        """   """
+        return self.body
+
+        
+
