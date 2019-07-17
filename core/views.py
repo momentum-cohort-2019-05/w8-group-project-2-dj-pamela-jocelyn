975178c0820b25cb87ce8f57c002f887fb29fb4f
polynova <jocelyncasanova1@gmail.com>
2019-07-17 16:44:52 -0400

    shows question and answers

--- core/views.py
+++ core/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from core.models import Question
 
 # Create your views here.
+
 def index(request):
-    return render(request, 'index.html')
+    questions = Question.objects.all()
+
+    context = {
+        'questions': questions
+    }
+
+    return render(request, 'index.html', context)
+
+def question_detail(request, pk):
+    question = Question.objects.get(pk=pk)
+
+    context = {
+        'question': question
+    }
+
+    return render(request, 'core/question_detail.html', context)
+
+
